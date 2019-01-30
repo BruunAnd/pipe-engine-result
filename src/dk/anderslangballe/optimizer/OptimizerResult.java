b7335c58f328be3bbc57229f26d9492349f68e95
Anders Jakobsen <andersjkbsn@gmail.com>
2019-01-30 14:31:41 +0100

    Added optimizer result metadata

--- src/dk/anderslangballe/optimizer/OptimizerResult.java
+++ src/dk/anderslangballe/optimizer/OptimizerResult.java
@@ -1,4 +1,16 @@
 package dk.anderslangballe.optimizer;
 
 public class OptimizerResult {
+    public String name;
+    public String newQuery;
+    public SimpleTree<String> plan;
+    public long loadingTime;
+    public long planningTime;
+    public long executionTime;
+    public List<Map<String, Object>> tuples;
+    public Set<String> bindingNames;
+
+    public OptimizerResult(String name) {
+        this.name = name;
+    }
 }
